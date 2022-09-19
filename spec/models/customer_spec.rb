d80bcc66be9db377bc4262394771c008264284f9
tjhaines-cap <tjhaines@eckerd.edu>
2022-09-19 17:22:08 -0600

    Feat: test and create tea model

--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -1,10 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe Customer do
-  it { should validate_presence_of :first_name }
-  it { should validate_presence_of :last_name }
-  it { should validate_presence_of :email }
-  it { should validate_presence_of :address }
+  describe 'associations' do
+    it { should have_many :subscriptions }
+  end
+
+  describe 'validations' do    
+    it { should validate_presence_of :first_name }
+    it { should validate_presence_of :last_name }
+    it { should validate_presence_of :email }
+    it { should validate_presence_of :address }
   
-  it { should validate_uniqueness_of :email }
+    it { should validate_uniqueness_of :email }
+  end
 end
