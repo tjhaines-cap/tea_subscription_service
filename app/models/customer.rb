9eb368f91dff12b37b18de8585cccea3c798ad2c
tjhaines-cap <tjhaines@eckerd.edu>
2022-09-20 15:48:04 -0600

    Revert "Feat/database setup"

--- app/models/customer.rb
+++
@@ -1,8 +0,0 @@
-class Customer < ApplicationRecord
-  has_many :subscriptions
-
-  validates :first_name, presence: true
-  validates :last_name, presence: true
-  validates :email, presence: true, uniqueness: true
-  validates :address, presence: true
-end
--- app/models/tea.rb
+++
@@ -1,8 +0,0 @@
-class Tea < ApplicationRecord
-  belongs_to :subscription
-
-  validates :title, presence: true
-  validates :description, presence: true
-  validates :temperature, presence: true
-  validates :brew_time, presence: true
-end
