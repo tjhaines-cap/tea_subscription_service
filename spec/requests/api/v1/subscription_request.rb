d9cc0694e5dc8fe6942ef7792e330e278232c59d
tjhaines-cap <tjhaines@eckerd.edu>
2022-09-20 19:46:10 -0600

    Feat: complete create method in subscriptions controller

--- spec/requests/api/v1/subscription_request.rb
+++ spec/requests/api/v1/subscription_request.rb
@@ -4,10 +4,10 @@ RSpec.describe 'Subscription Requests' do
 
   context 'happy path' do
     describe 'POST api/v1/subscription' do
-        
       it 'creates a subscription in the database associated with a customer and a tea' do
         customer = create(:customer)
         tea = create(:tea)
+        black_tea = create(:tea, title: "Black Tea")
         parameters = {
           title: 'Green Tea',
           price: 3.25,
@@ -19,10 +19,24 @@ RSpec.describe 'Subscription Requests' do
           post '/api/v1/subscriptions', params: parameters
         end.to change { Subscription.count }.by(1) 
 
-        json = JSON.parse(response.body, symbolize_names: true)
-        expect(response.status).to eq(400)
+        expect(response.status).to eq(201)
         expect(customer.subscriptions.length).to eq(1)
-        
+        expect(tea.subscriptions.length).to eq(1)
+
+        parameters2 = {
+          title: 'Black Tea',
+          price: 3.50,
+          status: 'active',
+          frequency: 'monthly',
+          customer_email: 'customer@email.com'
+        }
+
+        expect do
+          post '/api/v1/subscriptions', params: parameters2
+        end.to change { Subscription.count }.by(1)
+        expect(Customer.find(customer.id).subscriptions.length).to eq(2)
+        expect(tea.subscriptions.length).to eq(1)
+        expect(black_tea.subscriptions.length).to eq(1)
       end
     end 
   end
