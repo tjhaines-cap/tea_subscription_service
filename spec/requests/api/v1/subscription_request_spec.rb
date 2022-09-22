e246cc4e2f1ddcd7038111909c45bda6e9765874
tjhaines-cap <tjhaines@eckerd.edu>
2022-09-22 10:26:33 -0600

    Test: subscription cancellation patch request

--- spec/requests/api/v1/subscription_request_spec.rb
+++ spec/requests/api/v1/subscription_request_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe 'Subscription Requests' do
 
-  describe 'POST api/v1/subscriptions' do
+  describe 'POST api/v1/customers/:id/subscriptions' do
     context 'success' do
       it 'creates a subscription in the database associated with a customer and a tea' do
         customer = create(:customer)
@@ -41,7 +41,7 @@ RSpec.describe 'Subscription Requests' do
     end
   end
 
-  describe 'GET api/v1/subscriptions' do
+  describe 'GET api/v1/customers/:id/subscriptions' do
     context 'success' do
       it 'returns all subscriptions for a given customer' do
         customer = create(:customer)
@@ -57,6 +57,29 @@ RSpec.describe 'Subscription Requests' do
         data = json[:data] 
         expect(data).to be_a(Array)
         expect(data.length).to eq(2)
+      end
+    end
+  end
+
+  describe 'PATCH api/v1/customers/:id/subscriptions/:id' do
+    context 'success' do
+      it 'changes subscription status to cancelled' do
+        customer = create(:customer)
+        tea = create(:tea)
+        black_tea = create(:tea, title: "Black Tea")
+        subscription1 = create(:subscription, title: "Green Tea", tea_id: tea.id, customer_id: customer.id)
+        subscription2 = create(:subscription, title: "Black Tea", tea_id: black_tea.id, customer_id: customer.id)
+        
+        expect(subscription1.status).to eq("Active")
+
+        patch "/api/v1/customers/#{customer.id}/subscriptions/#{subscritpion1.id}"
+
+        expect(response.status).to eq(202)
+        json = JSON.parse(response.body, symbolize_names: true)
+        data = json[:data] 
+        expect(data).to be_a(Array)
+        expect(data.length).to eq(2)
+        expect(subscription1.status).to eq("Cancelled")
       end
     end
   end
