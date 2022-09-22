1a33fab19ab2b2808923dbda6b2f49efbde7454b
tjhaines-cap <tjhaines@eckerd.edu>
2022-09-22 09:42:29 -0600

    Feat: create seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Customer.create(first_name: "Jane", last_name: "Powell", email: "jpowell@email.com", address: "123 sonnybrook lane")
+Customer.create(first_name: "Judy", last_name: "Garland", email: "jgarland@email.com", address: "123 hollywood lane")
+
+Tea.create(title: "Green Tea", description: "refreshing green tea", temperature: 80, brew_time: 3)
+Tea.create(title: "Lavender Tea", description: "Soothing tea", temperature: 80, brew_time: 3)
+Tea.create(title: "Ginger Tea", description: "Good for colds", temperature: 70, brew_time: 4)
